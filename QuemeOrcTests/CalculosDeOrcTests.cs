70648389d255a76cb743165ca47ac2d86feb114f
jon <jones-ms@live.com>
2023-12-25 09:33:32 -0300

    feat: Adição de novo cenário de teste
    - Desenvolvimento de novo cenário de teste para a função que retorna o valor total do orçamento

--- QuemeOrcTests/CalculosDeOrcTests.cs
+++ QuemeOrcTests/CalculosDeOrcTests.cs
@@ -1,4 +1,5 @@
 using Queme.Models;
+using System.Globalization;
 
 namespace QuemeOrcTests
 {
@@ -14,12 +15,26 @@ namespace QuemeOrcTests
             decimal expectedResult = 1000m + 200m + 96.62m;
 
             //Act
-
             decimal result = CalculosDeOrcamento.ValorTotalOrcamento(servicos, custosAdicionais, incluirArt);
 
             //Assert
-
             Assert.Equal(expectedResult, result);
+        }
+        [Fact]
+        public void OrcamentoComImpostoSemDescontoEComissao()
+        {
+            //Arrange
+            decimal servicos = 2000;
+            decimal custosAdicionais = 554.44m;
+            bool incluirArt = true;
+            double imposto = 10.00;
+            decimal expectedResult = 2945.622222m;
+
+            //Act
+            decimal result = CalculosDeOrcamento.ValorTotalOrcamento(servicos, custosAdicionais, incluirArt, imposto);
+
+            //Assert
+            Assert.Equal(expectedResult.ToString("C", CultureInfo.GetCultureInfo("pt-BR")), result.ToString("C", CultureInfo.GetCultureInfo("pt-BR")));
         }
     }
 }
